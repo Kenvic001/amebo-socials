52c3d3c240eea885adad734d5e94db25927822ba
Kenvic001e <victorokeke64@gmail.com>
2023-07-11 12:05:06 -0700

    added backend url

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width='100%'
         height='auto'
         alt='advert'
-        src='http://localhost:3001/assets/ad.jpg'
+        src='https://amebosocials-api.onrender.com/assets/ad.jpg'
         style={{ borderRadius: '0.75rem', margin: '0.75rem 0' }}
       />
       <FlexBetween>
